39646b1dc72d315d1f4437c302f40e5cf2824761
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-04-14 14:41:17 -0700

    added cors

--- server/index.js
+++ server/index.js
@@ -2,12 +2,14 @@ require('newrelic');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const cors = require('cors');
 
 const controller = require('./controller.js');
 
 const port = 3001;
 const app = express();
 
+app.use(cors());
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
